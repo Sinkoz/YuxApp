5ad5ecf202ee71ce09879df75966bc277a14c4ec
Zhu Xinchen <Xinchen@Xinchens-MacBook-Pro.local>
2017-02-20 00:10:30 +0800

    first commit

---
+++ YuxApp/GalleryItem.swift
@@ -0,0 +1,9 @@
+//
+//  GalleryItem.swift
+//  YuxApp
+//
+//  Created by Zhu Xinchen on 15/2/17.
+//  Copyright © 2017 Zhu Xinchen. All rights reserved.
+//
+
+import Foundation
---
+++ YuxApp/WardrobeCell.swift
@@ -0,0 +1,13 @@
+//
+//  WardrobeCell.swift
+//  YuxApp
+//
+//  Created by Zhu Xinchen on 15/2/17.
+//  Copyright © 2017 Zhu Xinchen. All rights reserved.
+//
+
+import UIKit
+
+class WardrobeCell: UICollectionViewCell {
+    
+}
